aa006dc2870f4b608b9d3cec647c5c3f38aa3bde
ibra47122-max <ibra47122@gmail.com>
2025-10-13 16:37:36 +0200

    Add files via upload

---
+++ js/gestionPresupuesto.js
@@ -0,0 +1,56 @@
+// TODO: Variables globales
+let presupuesto = 0;
+
+// TODO: Funciones adicionales
+function actualizarPresupuesto(nuevoPresupuesto) {
+
+    if (nuevoPresupuesto > 0) {
+        presupuesto = nuevoPresupuesto
+        console.log(presupuesto)
+        return presupuesto
+    } else if (nuevoPresupuesto = 0){
+        return 0
+    } else if (nuevoPresupuesto < 0) {
+        alert("Debes introducir un valor positivo")
+        return -1
+    } else {
+        return -1
+    }
+}
+
+function mostrarPresupuesto() {
+    return `Tu presupuesto actual es de ${presupuesto} €`
+}
+
+function CrearGasto(descripcion, valor) {
+    this.descripcion = descripcion
+    if (valor >= 0) {
+        this.valor = valor
+    } else {
+        this.valor = 0
+    }
+}
+
+CrearGasto.prototype.mostrarGasto = function() {
+    return `Gasto correspondiente a ${this.descripcion} con valor ${this.valor} €`
+}
+
+CrearGasto.prototype.actualizarDescripcion = function(nuevaDescripcion) {
+    this.descripcion = nuevaDescripcion
+    return this.descripcion
+}
+
+CrearGasto.prototype.actualizarValor = function(nuevoValor) {
+    if (nuevoValor >= 0) {
+        this.valor = nuevoValor
+    }
+
+    return this.valor
+}
+
+// Exportación de funciones
+export   {
+    mostrarPresupuesto,
+    actualizarPresupuesto,
+    CrearGasto
+}
